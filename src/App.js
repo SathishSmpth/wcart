62a39c5fb6c7e68ecc02031a80fcb0a6c1e810da
SathishSmpth <sathishsmpth@outlook.com>
2022-12-09 22:38:35 +0530

    commit change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Navbar from "./components/navbar";
 import SectionOne from "./components/sectionOne";
-<<<<<<< HEAD
-// import SectionThree from "./components/sectionThree";
-// import SectionTwo from "./components/sectionTwo";
-// import SectionFour from "./components/sectionFour";
-// import SectionFive from "./components/sectionFive";
-=======
->>>>>>> 99a3de38000ff924f66147a5bae7028e6a88e7e3
+import SectionThree from "./components/sectionThree";
+import SectionTwo from "./components/sectionTwo";
+import SectionFour from "./components/sectionFour";
+import SectionFive from "./components/sectionFive";
 import Footer from "./components/footer";
 
 const App =()=> {
@@ -14,13 +11,10 @@ const App =()=> {
     <div className="app-container">
       <Navbar/>
       <SectionOne/>
-<<<<<<< HEAD
-      {/* <SectionTwo/>
+      <SectionTwo/>
       <SectionThree/>
       <SectionFour/>
-      <SectionFive/> */}
-=======
->>>>>>> 99a3de38000ff924f66147a5bae7028e6a88e7e3
+      <SectionFive/>
       <Footer/>
     </div>
   );
